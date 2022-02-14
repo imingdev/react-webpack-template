a107005d49dc4322eda27398f2a7618b680f473a
mingdev <mingdev@163.com>
2022-02-14 11:18:16 +0800

    update libs

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ const config = require('./build/config');
 module.exports = {
   presets: ['react-app'],
   plugins: [
-    '@rextjs/babel-plugin-auto-css-modules',
+    '@pieced/babel-plugin-auto-css-modules',
     config.isDevelopment && 'react-refresh/babel',
   ].filter(Boolean),
   compact: false,
